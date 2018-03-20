e2c99c38244107e6b5ccaea359fe3244c742d129
Ishay <ishay@Ishays-Mac.local>
2018-03-20 21:54:38 +0200

    arranged everything til the startNewRound Method

--- try to merge logic with blink saimon/ViewController.swift
+++ try to merge logic with blink saimon/ViewController.swift
@@ -7,34 +7,84 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    //1. all of the saimon playing buttons
+    //MARK:- 1. all of the saimon playing buttons
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var button4: UIButton!
     @IBOutlet weak var startButton: UIButton!
     
-    //2. the score lives related varibals
+    
+    //MARK:- 2. the score lives related varibals
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var livesLabel: UILabel!
-    
     var scoreNumber : Int = 0
     var numberOfLives = 3
+    var numberOfRounds : Int = 0 // might need to delete this it's unnessesery
     
     
+    //MARK:- 3. varibles related for playing the sounds and glow in the game
+    var gameTimer: Timer!
+    var soundPlayer : AVAudioPlayer!
+    var selectedSound : String = ""
+    let arrayOfSounds = ["buttonSound1", "buttonSound2", "buttonSound3", "buttonSound4"]
+    
+    //MARK:- 4. other varibles related to logic in the game
+    var randomNumber : Int = 0
+    var computerFinishedPlaying = false
+    var counterForTheComputerTurn : Int = 0
+    
+    var arrayOfBtnNumbersPlayedByTheComp: [Int] = [Int]()
+    var arrayOfBtnNumbersPlayedByThePlayer: [Int] = [Int]()
+    var arrayOfButtons : [UIButton] = []
+    
+    
+    //MARK:- 5. ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        arrayOfButtons = [button1, button2, button3, button4]
+        
+        //disable buttons
+        changeThePlayButtons(isEnabledStatus: false, arrayOfButtons: arrayOfButtons)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    
+    func changeThePlayButtons(isEnabledStatus: Bool, arrayOfButtons: [UIButton]){
+        for i in 0...arrayOfButtons.count - 1 {
+            arrayOfButtons[i].isEnabled = isEnabledStatus
+        }
+    }
+    
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        startButton.isEnabled = false
+        loadNewGame()
+    }
+    
+    func loadNewGame() {
+        print("start over started")
+        
+        arrayOfBtnNumbersPlayedByTheComp.removeAll()
+        scoreNumber = 0
+        numberOfRounds = 0
+        numberOfLives = 3
+        updateUI()
+        //startNewRound()
+    }
+    
+    func updateUI(){
+        scoreLabel.text = "score = \(scoreNumber)"
+        livesLabel.text = "Lives = \(numberOfLives)"
+    }
 
 }
 
