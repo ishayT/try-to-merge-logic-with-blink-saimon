44d77c7f0ddd5addf03bf9261ddf9520700741f7
Ishay <ishay@Ishays-Mac.local>
2018-03-20 16:01:54 +0200

    Initial Commit

---
+++ try to merge logic with blink saimon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  try to merge logic with blink saimon
+//
+//  Created by Ishay on 3/20/18.
+//  Copyright © 2018 Ishay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
